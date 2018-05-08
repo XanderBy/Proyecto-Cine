2f29613b61cf3eb369aa03f62f9c54c28daf5557
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-05-08 23:21:50 +0200

    0.0

---
+++ Proyecto-Cine/src/modelo/main.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import vista.IniciarSesion;
+
+public class main {
+
+	public static void main(String[] args) {
+		IniciarSesion is=new IniciarSesion();
+		is.setVisible(true);
+	}
+
+}

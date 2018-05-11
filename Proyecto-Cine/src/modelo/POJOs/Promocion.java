05baa93fe3e0f5a72ef86847f538e5bc7be902f1
Hector Cervera Panella <33516114+hcerpandam@users.noreply.github.com>
2018-05-11 09:22:26 +0200

    Implementada clase promoción

--- Proyecto-Cine/src/modelo/POJOs/Promocion.java
+++ Proyecto-Cine/src/modelo/POJOs/Promocion.java
@@ -4,6 +4,50 @@ public class Promocion {
 	
 	//VARIABLES
 	
+	private String descripcionPromo;
+	private int descuentoPromo;
+	
+	//CONSTRUCTORES
+	
+	Promocion(){
+		
+	}
+	
+	Promocion(String promoDescription, int promoDiscount){
+		
+		descripcionPromo=promoDescription;
+		descuentoPromo=promoDiscount;
+		
+	}
+	
+	//ACCEDENTES Y MUTADORES
+
+	public String getDescripcionPromo() {
+		return descripcionPromo;
+	}
+
+	public void setDescripcionPromo(String descripcionPromo) {
+		this.descripcionPromo = descripcionPromo;
+	}
+
+	public int getDescuentoPromo() {
+		return descuentoPromo;
+	}
+
+	public void setDescuentoPromo(int descuentoPromo) {
+		this.descuentoPromo = descuentoPromo;
+	}
+	
+	//MÉTODO TOSTRING
+	
+	@Override
+	
+	public String toString() {
+		return "Descripcion de la promoción: ".concat(getDescripcionPromo()) + "\nDescuento aplicable: "+ getDescuentoPromo();
+	}
+	
+	
+	
 	
 
 }

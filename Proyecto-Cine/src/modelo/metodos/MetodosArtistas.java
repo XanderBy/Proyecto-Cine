40d4f9a566a5082dbbecc6621220333b89520b60
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-05-11 17:03:46 +0200

    Metodos Artistas añadidos

--- Proyecto-Cine/src/modelo/metodos/MetodosArtistas.java
+++ Proyecto-Cine/src/modelo/metodos/MetodosArtistas.java
@@ -1,5 +1,25 @@
 package modelo.metodos;
 
-public class MetodosArtistas {
+import java.util.HashMap;
 
+import modelo.POJOs.Artista;
+import modelo.POJOs.Pais;
+import modelo.POJOs.Pelicula;
+
+public class MetodosArtistas {
+	HashMap<String, Artista> Artistas=new HashMap<String,Artista>();
+	public void CrearArtista(String nombreCompleto, Pais nacionalidad) {
+		Artista artista=new Artista(nombreCompleto, nacionalidad);
+		Artistas.put(nombreCompleto, artista);
+	}
+	public void EliminarArtista(String nombreCompleto) {
+		Artistas.remove(nombreCompleto);
+	}
+	public void AñadirADirector(String nombreCompleto, Pelicula pelicula) {
+		Artistas.get(nombreCompleto).PeliculasParticipaActor.put(nombreCompleto, pelicula);
+		
+	}
+	public void AñadirAActor(String nombreCompleto, Pelicula pelicula) {
+		Artistas.get(nombreCompleto).PeliculasParticipaDirector.put(nombreCompleto, pelicula);
+	}
 }

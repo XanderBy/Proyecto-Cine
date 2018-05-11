de1702dddcd06c89b8e297cb9358ced94aac2c93
Hector Cervera Panella <33516114+hcerpandam@users.noreply.github.com>
2018-05-11 11:48:42 +0200

    Deshechos cambios

--- Proyecto-Cine/src/modelo/metodos/MetodosCine.java
+++ Proyecto-Cine/src/modelo/metodos/MetodosCine.java
@@ -1,15 +1,5 @@
 package modelo.metodos;
 
-//Importamos PantallaAdministrador desde vista
-import vista.PantallaAdministrador;
-
 public class MetodosCine {
-	
-	//Instanciamos PantallaAdministrador
-	PantallaAdministrador pa;
-		
-	public void crearSala(String auditoriumName, String auditoriumCineId, int seatsNumber) {
-	
-	}
 
 }

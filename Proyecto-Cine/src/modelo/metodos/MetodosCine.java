3ecde8e6ee39b523406b95c692c69dafb54b983f
Hector Cervera Panella <33516114+hcerpandam@users.noreply.github.com>
2018-05-12 13:03:46 +0200

    Comenzados metodos cine

--- Proyecto-Cine/src/modelo/metodos/MetodosCine.java
+++ Proyecto-Cine/src/modelo/metodos/MetodosCine.java
@@ -1,5 +1,20 @@
 package modelo.metodos;
 
+import modelo.POJOs.Cine;
+import modelo.POJOs.Sala;
+
 public class MetodosCine {
+	
+	//CREAR SALA
+	
+	public void crearSala(Cine c, String auditoriumCineId, int seatsNumber) {
+		
+	}
+	
+	//ELIMINAR SALA
+	
+	public void eliminarSala(Cine c, Sala s) {
+		
+	}
 
 }

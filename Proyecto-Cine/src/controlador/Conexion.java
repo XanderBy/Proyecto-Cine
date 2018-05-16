1bae87fb6abdba1b55d7def90b056e97c8ebc051
Hector Cervera Panella <33516114+hcerpandam@users.noreply.github.com>
2018-05-16 17:23:45 +0200

    Modificado nombre BBDD en conexion

--- Proyecto-Cine/src/controlador/Conexion.java
+++ Proyecto-Cine/src/controlador/Conexion.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-	//Lo e cambiado de private a public
+	//Lo he cambiado de private a public
     public static Connection conex = null;
     
-    //1.1 CREAR CONEXION
+    //CREAR CONEXION
     public static Connection obtener() throws SQLException, ClassNotFoundException {
         if (conex == null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                conex = DriverManager.getConnection("jdbc:mysql://localhost:3306/cine", "root", "");//Puerto, Usuario, Contrasenia
+                conex = DriverManager.getConnection("jdbc:mysql://localhost:3306/bbddCine", "root", "");//Puerto, Usuario, Contrasenia
                 //(*) Obtener puerto: SHOW VARIABLES WHERE variable_name IN('hostname', 'port')
             } catch (SQLException ex) {
                 throw new SQLException(ex);

e03681c2ca74b6fd848269afd25a8c761b0959ad
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-05-16 19:09:32 +0200

    Algunos cambios

--- Proyecto-Cine/src/controlador/ControladorCine.java
+++ Proyecto-Cine/src/controlador/ControladorCine.java
@@ -8,11 +8,13 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import modelo.metodos.MetodosArtistas;
+import modelo.metodos.MetodosCine;
 import vista.PantallaAdministrador;
 
 public class ControladorCine implements ActionListener {// Esta Clase es una prueba
 	private PantallaAdministrador pantallaAdministrador;
 	private MetodosArtistas metodosArtistas;
+	private MetodosCine metodosCine;
 
 	public enum AccionMVC {// Aqui van las opciones
 		ELIMINAR_CINE, ANIADIR_CINE, MODIFICAR_CINE, ELEGIR_CINE
@@ -35,17 +37,15 @@ public class ControladorCine implements ActionListener {// Esta Clase es una pru
 		} catch (InstantiationException ex) {
 		} catch (IllegalAccessException ex) {
 		}
-		
+
 		// eliminar cine
 		this.pantallaAdministrador.jButton4.setActionCommand("ELIMINAR_CINE");
 		this.pantallaAdministrador.jButton4.addActionListener(this);
 
-		
 		// Añadir Cine
 		this.pantallaAdministrador.jButton3.setActionCommand("ANIADIR_CINE");
 		this.pantallaAdministrador.jButton3.addActionListener(this);
 
-		
 		// Elegir Cine
 		this.pantallaAdministrador.jButton2.setActionCommand("ELEGIR_CINE");
 		this.pantallaAdministrador.jButton2.addActionListener(this);
@@ -54,7 +54,7 @@ public class ControladorCine implements ActionListener {// Esta Clase es una pru
 		this.pantallaAdministrador.jButton6.setActionCommand("MODIFICAR_CINE");
 		this.pantallaAdministrador.jButton6.addActionListener(this);
 
-		}
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -62,9 +62,15 @@ public class ControladorCine implements ActionListener {// Esta Clase es una pru
 		switch (AccionMVC.valueOf(e.getActionCommand())) {
 		case ANIADIR_CINE:
 			// Aqui iría los metodos
+
+			metodosCine.crearCine(pantallaAdministrador.jTextField1.getText(),
+					pantallaAdministrador.jTextField2.getText(),
+					Integer.valueOf(pantallaAdministrador.jTextField3.getText()),
+					Integer.valueOf(pantallaAdministrador.jTextField4.getText()));
 			System.out.println("Hola");
 			break;
 		case ELEGIR_CINE:
+			//pantallaAdministrador.jList1.getSelectedValues()
 			break;
 		case MODIFICAR_CINE:
 			break;

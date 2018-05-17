2a7c8dbb62df856a6c24b40f3076d1fccfa35a2c
Antonio Calderon <carde46@hotmail.com>
2018-05-17 20:20:28 +0200

    metodo general añadido idem que el otro pero con la key int

--- Proyecto-Cine/src/modelo/metodos/MetodosGenerales.java
+++ Proyecto-Cine/src/modelo/metodos/MetodosGenerales.java
@@ -26,4 +26,26 @@ public class MetodosGenerales {
 		}
 		return resultado;
 	}
+	
+	public static boolean encuentraKeyIntHashMap(HashMap list, int key) {
+		
+		boolean resultado=false;
+		
+		try {
+			Iterator it = list.keySet().iterator();
+			while (it.hasNext()) {
+				int clave = (int) it.next();
+				if (clave==key) {
+					resultado=true;
+				}else{
+					resultado=false;
+				}
+			}
+		}catch (Exception e) {
+			System.out.println("Excepcion no controlada");
+			e.printStackTrace();
+			return resultado;
+		}
+		return resultado;
+	}
 }

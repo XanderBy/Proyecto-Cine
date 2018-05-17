e9183222d9e310ce22eb849c6e88a81a5204719b
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-05-18 00:54:51 +0200

    Cambios realizados

--- src/modelo/metodos/MetodosOpinion.java
+++ src/modelo/metodos/MetodosOpinion.java
@@ -3,6 +3,7 @@ package modelo.metodos;
 import java.time.LocalDate;
 
 import controlador.Conexion;
+import javax.swing.JOptionPane;
 import modelo.POJOs.CuentasAcceso;
 import modelo.POJOs.Opinion;
 import modelo.POJOs.ValoracionPeli;
@@ -10,11 +11,15 @@ import modelo.POJOs.ValoracionPeli;
 public class MetodosOpinion extends Conexion {
 	public void hacerOpinion(LocalDate fechaOpinion, String comentario, int idPelicula, ValoracionPeli valoracion,
 			int edadUsuario, CuentasAcceso usuario) {
-
+            if(fechaOpinion== null || comentario==null|| idPelicula==0 || valoracion==null|| edadUsuario==0||usuario==null){
+                JOptionPane.showMessageDialog(null, "No has introducido todos los valores");
+            }else{
 		Opinion opinion = new Opinion(fechaOpinion, comentario, idPelicula, valoracion, edadUsuario, usuario);
 		MetodosPelicula.peliculas.get(idPelicula).opiniones.put(usuario.getNombreAdminUsuario(), opinion);
 		// Anadir al navegar por un array de peliculas y introducir la opinion a la
 		// pelicula
+                JOptionPane.showMessageDialog(null, "Opinion realizada");
+            }
 	}
 
 	public void insertarOpinionBBDD() {

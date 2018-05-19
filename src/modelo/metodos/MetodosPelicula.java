64a31ba8c42cb0f00aff3568ff11bf321c5cc67c
Antonio Calderon <carde46@hotmail.com>
2018-05-19 21:13:49 +0200

    cambios en metodos pelicula
    metiendo la conexion etc

--- src/modelo/metodos/MetodosPelicula.java
+++ src/modelo/metodos/MetodosPelicula.java
@@ -1,10 +1,14 @@
 package modelo.metodos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Iterator;
 import javax.swing.JOptionPane;
+import controlador.ConexionManager;
 import modelo.POJOs.Artista;
 import modelo.POJOs.CalificacionEdades;
 import modelo.POJOs.GeneroPelicula;
@@ -16,9 +20,71 @@ public class MetodosPelicula {
 	
 	public static HashMap<Integer,Pelicula> peliculas = new HashMap<Integer,Pelicula>();
 	
+	public MetodosPelicula() {
+		
+	}
+	
+	public void cargarPeliculas() {
+		
+		
+		int agnoProduccion;
+		String titulo;
+		String tituloOriginal;
+		GeneroPelicula genero;
+		IdiomaOriginal idioma;
+		boolean subtitulosEs;
+		Pais paisOrigen;
+		String sitioWeb;
+		Duration duracion;
+		CalificacionEdades calificacionEdades;
+		LocalDate fechaEstrenoEs;
+		String resumen;
+		int idPelicula;
+		Pelicula P;
+		
+		try {
+			
+			ConexionManager conManager = new ConexionManager();
+			Connection conexion = conManager.crear();
+			
+			PreparedStatement consulta = conexion.prepareStatement("SELECT agnoProduccion,titulo,tituloOriginal,genero,idioma,subtitulos,paisOrigen,sitioWeb,duracionPelicula,calificacionEdades,fechaEstrenoEspagna,resumen,idPelicula FROM pelicula");
+			
+			ResultSet resultado = consulta.executeQuery();
+			
+			while (resultado.next()) {
+				agnoProduccion = resultado.getInt("agnoProduccion");
+				titulo = resultado.getString("titulo");
+				tituloOriginal = resultado.getString("tituloOriginal");
+				genero = GeneroPelicula.valueOf(resultado.getString("genero").toUpperCase());
+				idioma = IdiomaOriginal.valueOf(resultado.getString("idioma").toUpperCase());
+				subtitulosEs = resultado.getBoolean("subtitulos");
+				paisOrigen = Pais.valueOf(resultado.getString("paisOrigen").toUpperCase());
+				sitioWeb = resultado.getString("sitioWeb");
+				duracion = Duration.ofMinutes(resultado.getInt("duracionPelicula"));
+				calificacionEdades = CalificacionEdades.valueOf(resultado.getString("calificacionEdades").toUpperCase());
+				fechaEstrenoEs = resultado.getDate("fechaEstrenoEspagna").toLocalDate();
+				resumen = resultado.getString("resumen");
+				idPelicula = resultado.getInt("idPelicula");
+				
+				peliculas.put(idPelicula, new Pelicula(agnoProduccion,titulo,tituloOriginal,genero,idioma,subtitulosEs,paisOrigen,sitioWeb,duracion,calificacionEdades,fechaEstrenoEs,resumen,idPelicula));
+				
+			}
+			
+			conManager.cerrar();
+			
+		}catch(Exception e) {
+			System.err.println("Excepcion no controlada");
+			e.printStackTrace();
+		}
+	}
+	
 	public boolean agnadirPelicula(Pelicula p, int idPelicula) {
 
+		cargarPeliculas();
+		
 		try {
+			
+			
 			peliculas.put(idPelicula, p);
 			return true;
 			

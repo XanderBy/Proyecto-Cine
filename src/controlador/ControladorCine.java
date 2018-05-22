f58b1496e9eb3e0c0509916b84af8c3a878bc8c7
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-05-22 16:28:10 +0200

    ya carga promociones

--- src/controlador/ControladorCine.java
+++ src/controlador/ControladorCine.java
@@ -368,8 +368,14 @@ public class ControladorCine implements ActionListener, MouseListener {
 				// TODO Auto-generated catch block
 				e3.printStackTrace();
 			}
+			try {
+				metodosPromocion.cargarPromociones();
+			} catch (SQLException e3) {
+				// TODO Auto-generated catch block
+				e3.printStackTrace();
+			}
 			now = pantallaAdministrador.jTextField19.getText();
-			
+			System.out.println(MetodosSala.salas.size());
 			formatDateTime = LocalDateTime.parse(now, formatter);
 			System.out.println(formatDateTime);
 			System.out.println(pantallaAdministrador.jTextField20.getText());
